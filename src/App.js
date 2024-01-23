fc8f607c3cab67c87bb680cce633d71dd4dc0d53
Jimmy <jimin@Jimmys-MacBook-Air.local>
2024-01-23 14:14:49 +0300

    pdfdownload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+    <h2 className="text-rose-600">Hello</h2>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {Router,Routers} from 'react-router-dom';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ProductList from './pages/ProductList';
+import HomePage from './pages/HomePage';
+import ManufacturerList from './pages/ManufacturerList';
+import CategoryList from './pages/CategoryPage';
+import FirstPage from './pages/FirstPage';
+
+
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <FirstPage/>,
+  },
+  {
+    path: "/productlist",
+    element: <HomePage/>,
+  },
+  {
+    path: "/manufaclist/:manid",
+    element: <ManufacturerList/>,
+  },
+  {
+    path: "/catlist/:categoryname",
+    element: <CategoryList/>,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
