956559a31c3af8493bb80d37e72f65c940c5515c
Jimmy <96749667+JimmyCodeMunity@users.noreply.github.com>
2024-01-23 15:23:17 +0300

    Update ManufacturerList.jsx

--- src/pages/ManufacturerList.jsx
+++ src/pages/ManufacturerList.jsx
@@ -149,19 +149,73 @@ const ManufacturerList = () => {
   const zoomPluginInstance = zoomPlugin();
   const { ZoomInButton, ZoomOutButton, ZoomPopover } = zoomPluginInstance;
   return (
-    <div>
-      <h2>Start editing to see some magic happen {"\u2728"}</h2>
-      <h6>{manid}</h6>
+    <div class="bg-white">
+      <header class="absolute inset-x-0 top-0 z-50">
+        <nav
+          class="flex items-center justify-between p-6 lg:px-8"
+          aria-label="Global"
+        >
+          <div class="flex lg:flex-1">
+            <a href="#" class="-m-1.5 p-1.5">
+              <span class="text-slate-3xl">ResellerSprint</span>
+              
+            </a>
+          </div>
+          
+          <div class="hidden lg:flex lg:gap-x-12">
+            
+            <a href="#" class="text-sm font-semibold leading-6 text-gray-900">
+              Company
+            </a>
+          </div>
+          <div class="hidden lg:flex lg:flex-1 lg:justify-end">
+            <a href="#" class="text-sm font-semibold leading-6 text-gray-900">
+              Log in <span aria-hidden="true">&rarr;</span>
+            </a>
+          </div>
+        </nav>
 
-      {json.map((product)=>{
-        return(
-            <div>
-                <h6>{product.name}</h6>
+        
+      </header>
+
+      <div class="relative isolate px-6 pt-14 lg:px-8">
+        <div
+          class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          aria-hidden="true"
+        >
+          <div
+            class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+            
+          ></div>
+        </div>
+        <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+          <div class="hidden sm:mb-8 sm:flex sm:justify-center">
+            <div class="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+              Announcing our Products{" "}
+              <a href="#" class="font-semibold text-orange-600">
+                <span class="absolute inset-0" aria-hidden="true"></span>Read
+                more <span aria-hidden="true">&rarr;</span>
+              </a>
             </div>
-        )
-      })}
-      <button onClick={print}>print</button>
-      <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.8.335/build/pdf.worker.min.js">
+          </div>
+          <div class="text-center">
+            <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+              Daownload productlist for {manid}
+            </h1>
+            <div class="mt-10 flex items-center justify-center gap-x-6">
+              <a onClick={print}
+                href="#"
+                class="rounded-md bg-orange-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Download PDF
+              </a>
+              <a href="#" class="text-sm font-semibold leading-6 text-gray-900">
+                Learn more <span aria-hidden="true">→</span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.8.335/build/pdf.worker.min.js">
         <div
           style={{
             alignItems: "center",
@@ -178,6 +232,8 @@ const ManufacturerList = () => {
         </div>
         <Viewer fileUrl={url} plugins={[zoomPluginInstance]} />
       </Worker>
+        
+      </div>
     </div>
   );
 };
