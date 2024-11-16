55c717936446808d3dc7636bbb9a195d6ae68520
JimmyCodeMunity <jameswafula2002@gmail.com>
2024-11-16 11:03:39 +0300

    removed viewr

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -232,7 +232,7 @@ const CategoryList = () => {
           <ZoomPopover />
           <ZoomInButton />
         </div>
-        <Viewer fileUrl={url} plugins={[zoomPluginInstance]} />
+        {/* <Viewer fileUrl={url} plugins={[zoomPluginInstance]} /> */}
       </Worker>
         
       </div>
